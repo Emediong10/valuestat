e374521a7f1fcf0136661ed2a1e67fff72b63bb5
Emediong Bassey <basseyemediong@gmail.com>
2025-05-22 16:35:31 +0100

    Just corrected a little CSS issues on landing and About page

--- resources/views/components/filament-fabricator/page-blocks/about-second.blade.php
+++ resources/views/components/filament-fabricator/page-blocks/about-second.blade.php
@@ -1,15 +1,6 @@
+@aware(['page'])
 
- @aware(['page'])
-
-@props([
-    'main_heading',
-    'sub_heading',
-    'features',
-    'feature',
-    'image',
-    'title',
-    'description',
-])
+@props(['main_heading', 'sub_heading', 'features', 'feature', 'image', 'title', 'description'])
 
 <div class="py-0 my-0 bg-transparent section">
     <div class="container-fluid">
@@ -26,20 +17,19 @@
 
         @foreach ($features as $index => $feature)
             <div class="row align-items-md-center {{ $index % 2 !== 0 ? 'flex-md-row-reverse' : '' }}">
-                <div class="col-md-6 px-lg-0 min-vh-50 min-vh-lg-75 d-flex align-self-stretch"
-                     style="background:  url('{{ asset('storage/' . $feature['image']) }}')  no-repeat center center; background-size: cover;">
+                <div class="col-12 col-md-6 px-0 d-flex align-items-stretch"
+                    style="min-height: 250px; min-height: 50vh !important; background: url('{{ asset('storage/' . $feature['image']) }}') no-repeat center center; background-size: cover;">
                 </div>
-                <div class="p-5 col-md-6 p-lg-6">
+                <div class="p-5 col-12 col-md-6 p-lg-6">
                     <h3 class="h3 bottommargin fw-medium px-lg-5">{{ $index + 1 }}. {{ $feature['title'] }}</h3>
 
-                <div class="m-0 px-lg-5">
-                    {!! Str::markdown($feature['description']) !!}
-                </div>
-
+                    <div class="m-0 px-lg-5">
+                        {!! Str::markdown($feature['description']) !!}
+                    </div>
                 </div>
             </div>
             <div class="clear"></div>
         @endforeach
+
     </div>
 </div>
-
